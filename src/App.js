a93be79659f1a0b7a88ddc21733c7e8f5b6757c6
bomcrmon <110074089+bomcrmon@users.noreply.github.com>
2023-09-08 15:25:42 +0700

    context context2 header header2

--- src/App.js
+++ src/App.js
@@ -1,41 +1,71 @@
-import React from 'react'
-import { Header, Content, Footer } from './func-components'
-import Calendar from './class-components'
-import Banner from './banner'
-import {Calculator} from './calculator'
-import {Calculator2} from './calculator'
-import {EventData2,Table} from './event-data'
-import RefsFunc from './refs-func'
-import RefsArray from './refs-array'
-import MessageBox from './state-func'
+// import React from 'react'
+// // import { Header, Content, Footer } from './func-components'
+// import Calendar from './class-components'
+// import Banner from './banner'
+// import {Calculator} from './calculator'
+// import {Calculator2} from './calculator'
+// import {EventData2,Table} from './event-data'
+// import RefsFunc from './refs-func'
+// import RefsArray from './refs-array'
+// import MessageBox from './state-func'
+// import { userContext } from './context'
+// import Header from './context-header'
+// import Content from './context-content'
+// import Header2 from './context-header2'
+// import Content2 from './context-content2'
 
 
-function App() {
-    const divStyle={
-        color:'red',
-        backgroundColor:'powderblue',
-        fontSize:'larger',
-        padding:'3px',
-    }
-    return ( 
-        <>
-            {/* <Header/>
-            <p><center><Calendar/></center></p>
-            <Content/>
-            <h3 style={divStyle}> Hello World 
-                <br/> สวัดดีชาวโลก 
-            </h3>
-            <Footer/>
-            <Banner/>
-            <Calculator/> 
-            <Calculator2/> 
-            <EventData2/>
-            <Table/> 
-            <RefsFunc/> 
-            <RefsArray/>  */}
-            <MessageBox/> 
-        </>
+// function App() {
+//     const [user, setUser] = useState('')
+//     //  divStyle={
+//     //     color:'red',
+//     //     backgroundColor:'powderblue',
+//     //     fontSize:'larger',
+//     //     padding:'3px',
+//     // }
+//     return ( 
+//         <>
+//             {/* <Header/>
+//             <p><center><Calendar/></center></p>
+//             <Content/>
+//             <h3 style={divStyle}> Hello World 
+//                 <br/> สวัดดีชาวโลก 
+//             </h3>
+//             <Footer/>
+//             <Banner/>
+//             <Calculator/> 
+//             <Calculator2/> 
+//             <EventData2/>
+//             <Table/> 
+//             <RefsFunc/> 
+//             <RefsArray/> 
+//             <MessageBox/>  */}
+//             {/* <userContext.Provider value={'Tom Jerry'}>
+//                 <Header/>
+//                 <Content/>
+//             </userContext.Provider> */}
+//             <userContext.Provider value={[user, setUser]}>
+//                 <Header2 />
+//                 <Content2 />
+//             </userContext.Provider>
+//         </>
+//     );
+// }
+
+// export default App;
+
+import React, { useState } from 'react';
+import { userContext } from './context';
+import Header2 from './context-header2';
+import Content2 from './context-content2';
+
+export default function App() {
+    let [user, setUser] = React.useState('');
+
+    return (
+        <userContext.Provider value={[user, setUser]}>
+            <Header2 />
+            <Content2 />
+        </userContext.Provider>
     );
 }
-
-export default App;
---
+++ src/context-content2.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useContext } from './context';
+
+export default function Content2() {
+    let [user, setUser] = React.useState(''); // ใช้ useContext เพื่อรับค่า user และ setUser จาก context
+
+    const contentStyle = {
+        backgroundColor: '#ddd',
+        textAlign: 'center',
+        margin: 10,
+        padding: 10
+    };
+
+    const onClickSignin = (event) => {
+        event.preventDefault();
+        setUser('Bomcrmon'); // ตั้งค่า user เมื่อ Signin
+    };
+
+    return (
+        <div style={contentStyle}>
+            {
+                (user)
+                    ? <span>Hello {user}</span>
+                    : <span>Please <a href="/" onClick={onClickSignin}>Signin</a></span>
+            }
+        </div>
+    );
+}
+
+
+
+
